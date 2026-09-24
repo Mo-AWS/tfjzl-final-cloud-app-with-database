2bcf10a89ae12db1e0f6dc9da4e7818bdfafa45d
Mohammad <mhsrwcc@gmail.com>
2026-09-24 13:19:11 -0400

    Changes to admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,17 +4,17 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submi
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 2
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
 
 class ChoiceInLine(admin.StackedInline):
     model = Choice
     extra = 2
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -24,7 +24,7 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description']
 
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
+    inlines = [ChoiceInLine]
     list_display = ['content']
 
 class LessonAdmin(admin.ModelAdmin):
